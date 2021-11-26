70b365099c81a628c7cd94d9fee4c40f44076074
Vikram9940 <vickyeferley@gmail.com>
2021-11-26 15:01:18 +0530

    message

---
+++ src/ConvertionArray.java
@@ -0,0 +1,29 @@
+import java.util.Iterator;
+
+public class ConvertionArray {
+	public static void main(String[] args) {
+		int array[] = new int[3];
+		array[0] = 2;
+		array[1] = 3;
+		array[2] = 4;
+		
+		for (int i=0;i<array.length;i++) {
+			System.out.println(i);
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
